8b89083e9c02ff571bb6551518eecf639dbe9b75
Nils B <zero+github@nextunit.io>
2023-04-30 10:45:45 +0200

    Update op2aws.rb

--- op2aws.rb
+++ op2aws.rb
@@ -1,9 +1,9 @@
 class Op2aws < Formula
   desc "CLI Tool to use 1password for credentials to login into AWS"
   homepage "https://github.com/nextunit-io/op2aws"
-  version "0.1.0"
+  version "0.1.1"
   url "https://github.com/nextunit-io/op2aws/releases/download/#{version}/op2aws-darwin-amd64.zip"
-  sha256 "6dbfdb34649f6c0a5435556100debdd3a5489106a5e47cf9de08543c46cd0cf6"
+  sha256 "542274a38d144e9d242b48967b87cc48682aa2b9fe423d58c29da6ad8ee3363b"
   license "MIT"
 
   def install
